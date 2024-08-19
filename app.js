f1e13415f47d054a0402590eae4fcbfd49f1e5e8
GuyDav <guy.david@softwire.com>
2024-08-19 11:12:44 +0100

    LPARK-92: feat: using mailjet api instead of nodemailer

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const nodemailer = require('nodemailer');
-const mailjetTransport = require('nodemailer-mailjet-transport');
 const cors = require('cors');
 const handlebars = require('handlebars');
 const { promisify } = require('util');
 const fs = require('fs');
 const app = express();
 const port = 5000;
+const Mailjet = require('node-mailjet');
+const mailjet = Mailjet.apiConnect(
+    process.env.MAILJET_API_KEY,
+    process.env.MAILJET_SECRET_KEY,
+);
 
 app.use(cors());
 app.use(express.json({ limit: '25mb' }));
@@ -15,41 +18,56 @@ app.use(express.urlencoded({ limit: '25mb' }));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-});   
+});
 
-const readFile = promisify(fs.readFile);
+async function makeTemplate(name, paymentMethod) {
+  const readFile = promisify(fs.readFile);
 
-const transport = nodemailer.createTransport(mailjetTransport({
-  auth: {
-    apiKey: process.env.MAILJET_API_KEY,
-    apiSecret: process.env.MAILJET_SECRET_KEY,
+  let html = await readFile('emailTemplate.html', 'utf-8')
+  let template = handlebars.compile(html);
+  let data = {
+    name: name,
+    paymentMethod: paymentMethod
   }
-}))
 
-async function sendEmail({ email, name, paymentMethod }) {
-    let html = await readFile('emailTemplate.html', 'utf-8')
-    let template = handlebars.compile(html);
-    let data = {
-      name: name,
-      paymentMethod: paymentMethod
-    }
-    let htmlToSend = template(data);
-
-    const mail = {
-      from: 'aridem.test01@gmail.com',
-      to: email,
-      subject: 'Parking Confirmation',
-      html: htmlToSend,
-    };
-
-    try {
-      const info = await transport.sendMail(mail);
-      console.log(info);
-    } catch (error) {
-      console.log(error)
-    }
+  return template(data);
 }
-  
+
+async function sendEmail(email, name, paymentMethod) {
+  let htmlToSend = await makeTemplate(name, paymentMethod);
+  email = String(email);
+
+  const request = mailjet
+          .post('send', { version: 'v3.1' })
+          .request({
+            Messages: [
+              {
+                From: {
+                  Email: "aridem.test01@gmail.com",
+                  Name: "Test email"
+                },
+                To: [
+                  {
+                    Email: 'guy.david@softwire.com',
+                    Name: "Parking User"
+                  }
+                ],
+                Subject: "Parking confirmaton",
+                TextPart: "test",
+                HTMLPart: htmlToSend
+              }
+            ]
+          })
+
+  request
+      .then((result) => {
+          console.log(result.body)
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+}
+
 app.get("/", (req, res) => {
   sendEmail(req.query)
     .then((response) => res.send(response.message))
